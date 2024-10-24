b702ea895bf77bbb96d705b43e3ddbd1571bd833
irfanrizqinr <irfanrizqi2005@gmail.com>
2024-10-24 23:53:05 +0700

    memperbaiki routing url -Irfan

--- jogjappetite/urls.py
+++ jogjappetite/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ratings/', include('ratings.urls')),   # Mengarahkan ke URLs di aplikasi 'ratings'
-
+    path('favorite', include('favorite.urls')),  # Mengarahkan URLs di aplikasi 'favorite'
 
     path('ratings/', include('ratings.urls')), 
     path('', include('explore.urls')),  
