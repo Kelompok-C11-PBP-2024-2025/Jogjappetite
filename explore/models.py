2aad1ae39fe00494729bf5595b7f5038812131cc
Sulthan Adrin <sulthanadrin0806@gmail.com>
2024-10-25 23:29:43 +0700

    CRUD KELARRRR AKHIRNYA (kayanya)

--- explore/models.py
+++ explore/models.py
@@ -2,9 +2,15 @@ from django.utils import timezone
 from django.db import models
 from ratings.models import Menu, Restaurant
 from authentication.models import UserProfile
+from django.contrib.auth.models import User
 
-class ExploreHistory(models.Model):
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='explore_history')
-    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, related_name='menu_history')
-    clicked_at = models.DateTimeField(default=timezone.now)
-    
+class Bookmark(models.Model):
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+        added_on = models.DateTimeField(auto_now_add=True)
+
+        class Meta:
+            unique_together = ('user', 'menu')  # User hanya bisa bookmark menu satu kali
+
+        def __str__(self):
+            return f"{self.user.username} bookmarked {self.menu.nama_menu}"
