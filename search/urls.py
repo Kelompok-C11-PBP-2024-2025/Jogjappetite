6aa60a47a6c08af7b1753cc2b68f4e1b804e9d8d
FilbertAT <filbertaurelian@gmail.com>
2024-12-08 16:14:23 +0700

    Integration with Flutter for Search Module

--- search/urls.py
+++ search/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
-from search.views import food_search, resto_search, save_search_history, delete_search_history, get_search_history, show_json
+from search.views import *
 
 app_name = 'search'
 
 urlpatterns = [
     path("food-search/", food_search, name="food_search"),
     path("resto-search/", resto_search, name="resto_search"),
+    path("food-search-flutter/", food_search_flutter, name="food_search_flutter"),
+    path("resto-search-flutter/", resto_search_flutter, name="resto_search_flutter"),
     path('save-search/', save_search_history, name='save_search_history'),
+    path('save_search_history_flutter/', save_search_history_flutter, name='save_search_history_flutter'),
     path('delete-history/<int:history_id>/', delete_search_history, name='delete_search_history'),
     path('get-search-history/', get_search_history, name='get-search-history'),
-    path('show-json/', show_json, name='show_json'),
+    path('json/', show_json, name='show_json'),
 ]
